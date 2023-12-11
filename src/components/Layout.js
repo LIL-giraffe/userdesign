e0b9cd2bbb88114729b184f7d71b48a8f5f32337
LIL-Giraffe <hdbest33@gmail.com>
2023-12-11 19:39:25 +0530

    layout without content

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,21 +1,22 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Sidebar from './Sidebar'
-import {Outlet} from 'react-router-dom'
+import React from "react";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import { Outlet } from "react-router-dom";
 
 // className='flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden'
+// {/* <div className="flex justify-center justify-items-center w-full"> */}
 
 const Layout = () => {
   return (
-    <div className='flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden' >
-       <Sidebar/>
-       
-       <div className='w-full' >
-       <Navbar/>
-       {<Outlet/>} 
-       </div>
-    </div>
-  )
-}
+    <div className="flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden">
+      <Sidebar />
 
-export default Layout
+      <div className="w-full">
+        <Navbar />
+        {<Outlet />}
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Statistics = () => {
   return (
-    <div>Statistics</div>
-  )
-}
+    <div className="flex flex-wrap w-full justify-center justify-items-center  h-[600] m-4 ">
+      <div className="w-full h-1/2 flex flex-row">
+        <div className="border-2 w-[580px] h-[280px]  shadow-lg bg-slate-300 rounded-lg">
 
-export default Statistics
+        </div>
+        <div className="border-2 w-[580px] h-[280px] shadow-lg bg-slate-300 rounded-lg">
+
+        </div>
+      </div>
+      <div className="w-full h-1/2 flex flex-row">
+        <div className="border-2 w-[580px] h-[160px] shadow-lg bg-slate-300 rounded-lg">
+
+        </div>
+        <div className="border-2 w-[580px] h-[280px] shadow-lg bg-slate-300 rounded-lg">
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Statistics;
